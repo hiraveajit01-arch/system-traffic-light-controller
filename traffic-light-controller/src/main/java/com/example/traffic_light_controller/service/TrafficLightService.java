da9b0bb8b23fb880a9650f16e6280b1b3f7f35fd
Ajit Hirave <hiraveajit01@gmail.com>
2026-02-22 15:24:53 +0530

    Integrated History service with traffic light service

--- traffic-light-controller/src/main/java/com/example/traffic_light_controller/service/TrafficLightService.java
+++ traffic-light-controller/src/main/java/com/example/traffic_light_controller/service/TrafficLightService.java
@@ -1,16 +1,19 @@
 package com.example.traffic_light_controller.service;
 
 import com.example.traffic_light_controller.entity.Direction;
+import com.example.traffic_light_controller.entity.TrafficHistory;
 import com.example.traffic_light_controller.entity.TrafficLightState;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 @Service
 public class TrafficLightService {
     private final ConflictValidator validator = new ConflictValidator();
+    private final HistoryService historyService = new HistoryService();
 
     private final Map<Direction, TrafficLightState> states =
             new EnumMap<>(Direction.class);
@@ -25,11 +28,23 @@ public class TrafficLightService {
     }
 
 
-    public void changeState(Direction direction, TrafficLightState newState) {
+
+    public synchronized void changeState(Direction direction, TrafficLightState newState) {
+
+        TrafficLightState previousState = states.get(direction);
 
         validator.validate(direction, newState, states);
+
         states.put(direction, newState);
+
+        historyService.record(direction, previousState, newState);
     }
+
+    public List<TrafficHistory> getHistory() {
+        return historyService.getHistory();
+    }
+
+
 
 
 }
